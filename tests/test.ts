faff62bc5ece179699d9aab21c33380fe3b123fd
Mario Buonomo <ma.buonomo@gmail.com>
2018-09-19 23:47:31 +0200

    improve

--- tests/test.ts
+++ tests/test.ts
@@ -1,10 +1,9 @@
-var assert = require('assert');
-// var describe = require('describe');
+import { expect } from 'chai';
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            expect(-1).to.equal([1, 2, 3].indexOf(4));
+        });
     });
-  });
 });

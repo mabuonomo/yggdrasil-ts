99549bd1afe5a36de258e931a35c8df86d5ec17b
Mario Buonomo <ma.buonomo@gmail.com>
2018-10-08 21:50:16 +0200

    tests improve

--- tests/user.ts
+++ tests/user.ts
@@ -2,19 +2,27 @@ import { expect } from 'chai';
 import { UserModel } from "../src/models/userModel";
 import { createConnection } from "typeorm";
 
-const ormconfig = require("../ormconfig.json");
+const config = require("../config.json");
 
 describe('User', function () {
+    describe('User init', function () {
+        it('should return _id is undefined', function () {
+            var user = new UserModel();
+            console.log(user._id);
+            expect(user._id).undefined;
+        });
+    });
+
     describe('User create', function () {
-        it('should return _id > 0', async function () {
+        it('should return _id is not undefined', function () {
 
             createConnection({
-                type: ormconfig.type,
-                host: ormconfig.host,
-                port: ormconfig.port,
-                username: ormconfig.username,
-                password: ormconfig.password,
-                database: ormconfig.database,
+                type: config.ormconfig.type,
+                host: config.ormconfig.host,
+                port: config.ormconfig.port,
+                username: config.ormconfig.username,
+                password: config.ormconfig.password,
+                database: config.ormconfig.database,
                 entities: [
                     UserModel
                 ],
@@ -24,7 +32,7 @@ describe('User', function () {
                 // here you can start to work with your entities
                 var user = new UserModel();
                 var user_saved = await connection.manager.save(user);
-                expect(user_saved._id).not.null;
+                expect(user_saved._id).not.undefined;
 
             }).catch(error => console.log(error));
 
